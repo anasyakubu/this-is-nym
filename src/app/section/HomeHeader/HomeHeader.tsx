7ba9f4196a110f2a359bd2d505459680a2f6a7f5
anasyakubu <yakubuanas04@gmail.com>
2023-12-05 19:19:34 +0100

    HomeHeader Design Change and svg icon added

--- src/app/section/HomeHeader/HomeHeader.tsx
+++ src/app/section/HomeHeader/HomeHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Nav from "../../components/Nav";
 import "./HomeHeader.scss";
 import Image from "next/image";
-import PhoneDashBoard from "../../../../public/nym.png";
+import PhoneDashBoard from "../../../../public/Programmer-pana.svg";
 import PrimaryBtn from "@/app/components/PrimaryBtn";
 // import { BsAirplaneEnginesFill } from "react-icons/bs";
 //import { Fade } from "react-awesome-reveal";
@@ -20,31 +20,33 @@ const HomeHeader = () => {
       <div className="lg:px-6">
         <div className="p-3 space-y-2 lg:grid lg:grid-cols-2 lg:gap-x-6 lg:space-y-0">
           {/* py-24 sm:py-0 */}
-          <div className="text-center mt-10 pt-10">
-            <Image className="phoneImg" src={PhoneDashBoard} alt="Logo" />
-          </div>
           <div className="pl-3 max-w-xl lg:py-16">
             {/* sm:py-48 lg:py-56 */}
             <div className="text">
               <h1
-                className="text-2xl font-extrabold tracking-wide shadow-2 sm:text-4xl"
+                className="text-md font-extrabold tracking-wide shadow-2 sm:text-3xl"
                 style={{ color: "#1D4ED8" }} //#122231
               >
-                RUNNING YOUR SCHOOL SMARTLY
+                THE BEST CHOICE FOR IT SOLUTIONS
               </h1>
               <p
-                className="mt-4 text-md leading-8"
-                style={{ color: "#122231" }} //#3d4f60
+                className="mt-4 text-sm leading-8"
+                style={{ color: "#122231" }} //#3d4f60 122231
               >
-                We understand that the safety and well-being of your child are
-                paramount, even when theyre at school. Thats why our schools kid
-                monitoring software goes above and beyond to ensure parents are
-                always informed about their childs arrival and departure.
+                At NYM Technologies Limited, we are at the forefront of
+                innovation, driving technological advancements across various
+                domains. As a leading technology company, we specialize in AI,
+                Software Development, Consultancy, Robotics, and Web
+                Development, delivering cutting-edge solutions that empower
+                businesses to thrive in the digital age.
               </p>
             </div>
             <div className="flex mt-10">
               <PrimaryBtn text="Get your school on board" />
             </div>
+          </div>
+          <div className="text-center mt-10 pt-10">
+            <Image className="phoneImg" src={PhoneDashBoard} alt="Logo" />
           </div>
         </div>
         <div className="p-5">
